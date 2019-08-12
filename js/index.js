791779727a718b250dce877b4e95addc759c786e
remixj <jianghongxiang@bupt.edu.cn>
2019-08-12 16:43:02 +0800

    add the popover function to display information of classmates and still need to be altered to meet the needs. Can call the function  to create a popover in specific location. and need to input the CN Province name as CN_area parameter

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
+//                                                    670 0
+//               261 80                                         870 106
+//                                  528 200
+//         80 264                                          720 264
+//
+//                420 517
+//                                                        710 502
+//
+//                                544 569
+//
+//  under the width of 955
+
 // creat a hash set to restore the area the user just selected
 var selected_area_code = new Set();
-
+var count = 1;
 // fill color select
 $("#inputGroupSelect01").change(function(selection) {
   change_map_fill_color(selection.target.value);
@@ -21,20 +33,27 @@ function change_map_boarder_color(boarder_color) {
 
 $("path").click(function(click) {
   // click.target.id -> CN-65
+  var div_element = $(this).parent().parent();
+  var div_x = div_element.offset().left,
+    div_y = div_element.offset().top;
+
   var svg_element = $(this).parent();
   // svg_x -> svg element to the left boarder of website
   // svg_y -> svg element to the top boarder of website
   var svg_x = svg_element.offset().left,
     svg_y = svg_element.offset().top;
+  // mouse_x -> mouse click posision to the left boarder of website
+  // mouse_y -> mouse click position to the top boarder of website
   var mouse_x = click.pageX,
     mouse_y = click.pageY;
-
-  console.log(mouse_x - svg_x, mouse_y - svg_y);
-  create_line(mouse_x - svg_x, mouse_y - svg_y);
-  create_circle(mouse_x - svg_x, mouse_y - svg_y);
-
   var CN_code = click.target.id;
   $("#" + CN_code).toggleClass("selected");
+  var CN_area = $("#"+CN_code).attr("title");
+  console.log(CN_area);
+  // create_circle_outside_svg(mouse_x - div_x, mouse_y - div_y);
+  create_popover_outside_svg(mouse_x - div_x, mouse_y - div_y, CN_area);
+  // create_line_inside_svg(mouse_x - svg_x, mouse_y - svg_y);
+  // create_circle_inside_svg(mouse_x - svg_x, mouse_y - svg_y);
   // update the hash set
   if (selected_area_code.has(CN_code)) {
     selected_area_code.delete(CN_code);
@@ -43,7 +62,38 @@ $("path").click(function(click) {
   }
 });
 
-function create_circle(starting_x, starting_y) {
+function create_circle_outside_svg(x, y) {
+  var circle_code = '<div class="circle-base" style="left : ' + x + 'px; top : ' + y + 'px; "></div>';
+  $(".map-svg").prepend(circle_code);
+}
+
+// generate popover outside svg in the map-svg div tag
+function create_popover_outside_svg(x, y, CN_area) {
+  var user_id = 'user_' + String(count);
+  var code = generate_popover_code(x, y, user_id, CN_area);
+
+  $(".map-svg").prepend(code);
+  // to make sure every pop over tag could be generate popover correctly
+  $(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+  });
+  $("#" + user_id).attr("data-content", "<em>Hello</em> world<img class='classmate-img' src='img/" + user_id + ".jpg'>");
+  count++;
+}
+
+// generate popover code
+function generate_popover_code(x, y, user_id, CN_area) {
+  // popover title should contain information like position may be the province the user submitted
+  var popover_title = 'popover - title';
+  // the most important thing is to define the structure of popover content html
+  var img_path = "img/" + user_id + ".jpg";
+
+  // and we add the popover code into the map
+  return '<a role="button" id="' + user_id + '" data-html = "true" class="circle-base" style="left : ' + x + 'px ; top : ' + y + 'px; " data-toggle="popover" data-placement="top" data-container=".map-svg" data-title="'+ CN_area +'" ></div>';
+}
+
+// create a circle inside svg
+function create_circle_inside_svg(starting_x, starting_y) {
   var circle = makeSVG('circle', {
     cx: Number(starting_x),
     cy: Number(starting_y),
@@ -55,7 +105,8 @@ function create_circle(starting_x, starting_y) {
   document.getElementById('china-map').appendChild(circle);
 }
 
-function create_line(starting_x, starting_y){
+// create a line inside svg
+function create_line_inside_svg(starting_x, starting_y) {
   //<path d="m 610,406 40,0" stroke="red" fill="red"></path>
   var line_code = "m " + starting_x + "," + starting_y + " 10" + "," + "10";
   var line = makeSVG('path', {
@@ -65,10 +116,6 @@ function create_line(starting_x, starting_y){
     "stroke-width": 3
   });
   document.getElementById('china-map').appendChild(line);
-}
-
-function generate_proper_line_code(starting_x, starting_y) {
-
 }
 
 function makeSVG(tag, attrs) {
