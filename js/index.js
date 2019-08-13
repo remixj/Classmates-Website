2c4c6c18a6f6be60d04a20c767adf591736deb0d
remixj <jianghongxiang@bupt.edu.cn>
2019-08-13 21:20:01 +0800

    specified function of generate code for popover content and clearified the user information by using a hash map and you can check inside the index.js file

--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ $("path").click(function(click) {
     mouse_y = click.pageY;
   var CN_code = click.target.id;
   $("#" + CN_code).toggleClass("selected");
-  var CN_area = $("#"+CN_code).attr("title");
+  var CN_area = $("#" + CN_code).attr("title");
   console.log(CN_area);
   // create_circle_outside_svg(mouse_x - div_x, mouse_y - div_y);
   create_popover_outside_svg(mouse_x - div_x, mouse_y - div_y, CN_area);
@@ -77,8 +77,122 @@ function create_popover_outside_svg(x, y, CN_area) {
   $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
   });
-  $("#" + user_id).attr("data-content", "<em>Hello</em> world<img class='classmate-img' src='img/" + user_id + ".jpg'>");
+  // we should get user informaion from database and it should contain the basic information like that
+  var user_hashmap = {
+    user_hash_id: "asfsfasf", // should be a unique id for a user
+    user_img: "img/user_1.jpg",
+    user_name: "Bruce Duan",
+    user_gender: "male",
+    user_email: "dd0elx@qq.com",
+    user_tele: "+86 13567960810",
+    user_DoB: "1997.01.14",
+    user_status: false, // true -> student false -> employed
+    user_subordin: "Beijing University of Posts and Telecommunications"
+  };
+  var display_info = ["user_img", "user_name", "user_gender", "user_email", "user_tele", "user_DoB", "user_status", "user_subordin"];
+  var display_opt =  [   true   ,     true   ,     true     ,     true    ,     true   ,    false  ,       true   ,      true      ];
+  var display_arr = [];
+  for (var i = 0; i < display_info.length; i++) {
+    if (display_opt[i])
+      display_arr.push(display_info[i]);
+  }
+  $("#" + user_id).attr("data-content", generate_information(display_arr, user_hashmap));
+  // $("#" + user_id).attr("data-content", "<em>Hello</em> world<img class='classmate-img' src='img/" + user_id + ".jpg'>");
   count++;
+}
+
+function generate_information(display_arr, user_hashmap) {
+  /*
+  <div class="popover-body">
+      <div>
+      	<img class="classmate-img" src="img/user_1.jpg">
+      	<b>Emily Li</b>
+          <i class="fas fa-venus" aria-hidden="true"></i>
+      </div>
+      <div>
+          <p><i class="fas fa-envelope" aria-hidden="true"></i>  dd0exclu@163.com</p>
+      </div>
+      <div>
+          <p><i class="fas fa-phone" aria-hidden="true"></i>  +86 13567960810</p>
+      </div>
+      <div>
+      	<p><i class="fas fa-birthday-cake" aria-hidden="true"></i>  1997 01 14</p>
+      </div>
+      <div>
+      	<p></p>
+      </div>
+      <div>
+      	<p><i class="fas fa-graduation-cap" aria-hidden="true"></i>  BUPT</p>
+      </div>
+  </div>
+  */
+  var popover_content = '<div>';
+  var stack = ['div'];
+  for (var i = 0; i < display_arr.length; i++) {
+    var user_item = display_arr[i];
+    switch (user_item) {
+      case "user_img":
+        popover_content += '<img class="classmate-img" src="' + user_hashmap[user_item] + '">';
+        break;
+      case "user_name":
+        popover_content += '<b>';
+        stack.push('b');
+        popover_content += user_hashmap[user_item];
+        popover_content += '</' + stack.pop() + '>';
+        break;
+      case "user_gender":
+        if (user_hashmap[user_item] === "male") {
+          popover_content += '<i class="fas fa-mars" aria-hidden="true"></i>';
+        } else {
+          popover_content += '<i class="fas fa-venus" aria-hidden="true"></i>';
+        }
+        popover_content += '</' + stack.pop() + '>';
+        break;
+      case "user_email":
+        stack.push('div');
+        stack.push('p');
+        popover_content += '<div><p>';
+        popover_content += '<i class="fas fa-envelope" aria-hidden="true"></i>' + user_hashmap[user_item];
+        popover_content += '</' + stack.pop() + '>';
+        popover_content += '</' + stack.pop() + '>';
+        break;
+      case "user_tele":
+        stack.push('div');
+        stack.push('p');
+        popover_content += '<div><p>';
+        popover_content += '<i class="fas fa-phone" aria-hidden="true"></i>' + user_hashmap[user_item];
+        popover_content += '</' + stack.pop() + '>';
+        popover_content += '</' + stack.pop() + '>';
+        break;
+      case "user_DoB":
+        stack.push('div');
+        stack.push('p');
+        popover_content += '<div><p>';
+        popover_content += '<i class="fas fa-birthday-cake" aria-hidden="true"></i>' + user_hashmap[user_item];
+        popover_content += '</' + stack.pop() + '>';
+        popover_content += '</' + stack.pop() + '>';
+        break;
+      case "user_status":
+        stack.push('div');
+        stack.push('p');
+        popover_content += '<div><p>';
+        if (user_hashmap[user_item] === true){
+          popover_content += '<i class="fas fa-graduation-cap" aria-hidden="true"></i>';
+        }
+        else{
+          popover_content += '<i class="fas fa-building" aria-hidden="true"></i>';
+        }
+        break;
+      case "user_subordin":
+        popover_content += user_hashmap[user_item];
+        popover_content += '</' + stack.pop() + '>';
+        popover_content += '</' + stack.pop() + '>';
+        break;
+      default:
+        console.log(user_item);
+    }
+  }
+  return popover_content;
 }
 
 // generate popover code
@@ -89,7 +203,7 @@ function generate_popover_code(x, y, user_id, CN_area) {
   var img_path = "img/" + user_id + ".jpg";
 
   // and we add the popover code into the map
-  return '<a role="button" id="' + user_id + '" data-html = "true" class="circle-base" style="left : ' + x + 'px ; top : ' + y + 'px; " data-toggle="popover" data-placement="top" data-container=".map-svg" data-title="'+ CN_area +'" ></div>';
+  return '<a role="button" id="' + user_id + '" data-html = "true" class="circle-base" style="left : ' + x + 'px ; top : ' + y + 'px; " data-toggle="popover" data-placement="top" data-container=".map-svg" data-title="' + CN_area + '" ></div>';
 }
 
 // create a circle inside svg
